f8b82df839d649aa8bc6122e90bf0cf439650f17
James Carman <jcarman@callibrity.com>
2022-09-17 00:37:17 -0400

    Adding more worker threads.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 # Start server
 echo "Starting server"
-gunicorn bakeoff_python_django.wsgi:application --bind 0.0.0.0:$PORT --workers=3
+gunicorn bakeoff_python_django.wsgi:application --bind 0.0.0.0:$PORT --workers=10
